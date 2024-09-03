8fa29436094d2dd4c89555e3c22a6d0101c9b93d
ekkx <geniusnaru@gmail.com>
2024-09-03 12:06:15 +0900

    refactor: use strconv to build endpoint

--- endpoints.go
+++ endpoints.go
@@ -1,5 +1,7 @@
 package yaygo
 
+import "strconv"
+
 var (
 	EndpointAPI = "https://api.yay.space/"
 	EndpointV1  = EndpointAPI + "v1/"
@@ -34,11 +36,16 @@ var (
 	EndpoinWalletV1         = EndpointV1 + "wallet/"
 	EndpintThreadsV1        = EndpointUsersV1 + "threads/"
 
-	EndpointUsersRegister                                = func() string { return EndpointUsersV3 + "register" }
-	EndpointUsersContactFriends                          = func() string { return EndpointUsersV1 + "contact_friends" }
-	EndpointUsersDeleteFootprint                         = func(uID string, fuID string) string { return EndpointUsersV2 + uID + "/footprints/" + fuID }
+	EndpointUsersInvitationCode  = func(code string) string { return EndpointUsersV1 + "invitation_code" }
+	EndpointUsersRegister        = func() string { return EndpointUsersV3 + "register" }
+	EndpointUsersContactFriends  = func() string { return EndpointUsersV1 + "contact_friends" }
+	EndpointUsersDeleteFootprint = func(uID int, fpID int) string {
+		return EndpointUsersV2 + strconv.Itoa(uID) + "/footprints/" + strconv.Itoa(fpID)
+	}
 	EndpointUsersDestroyUser                             = func() string { return EndpointUsersV2 + "destroy" }
-	EndpointUsersFollowUser                              = func(uID string) string { return EndpointUsersV2 + uID + "/follow" }
+	EndpointUsersSecretDisable                           = func() string { return EndpointUsersV1 + "secret/disable" }
+	EndpointUsersSecretEnable                            = func() string { return EndpointUsersV1 + "secret/enable" }
+	EndpointUsersFollowUser                              = func(uID int) string { return EndpointUsersV2 + strconv.Itoa(uID) + "/follow" }
 	EndpointUsersFollow                                  = func() string { return EndpointUsersV2 + "follow" }
 	EndpointUsersActiveFollowings                        = func() string { return EndpointUsersV1 + "active_followings" }
 	EndpointUsersAdditionalSettings                      = func() string { return EndpointUsersV1 + "additonal_notification_setting" }
@@ -50,18 +57,18 @@ var (
 	EndpointUsersFollowRequestsCount                     = func() string { return EndpointUsersV2 + "follow_requests_count" }
 	EndpointUsersFollowingBornToday                      = func() string { return EndpointUsersV2 + "following_born_today" }
 	EndpointUsersFootprints                              = func() string { return EndpointUsersV2 + "footprints" }
-	EndpointUsersFresh                                   = func(uID string) string { return EndpointUsersV2 + "fresh/" + uID }
+	EndpointUsersFresh                                   = func(uID int) string { return EndpointUsersV2 + "fresh/" + strconv.Itoa(uID) }
 	EndpointUsersHimaUsers                               = func() string { return EndpointUsersV2 + "hima_users" }
-	EndpointUsersFollowRecommended                       = func(uID string) string { return EndpointUsersV1 + uID + "/follow_recommended" }
+	EndpointUsersFollowRecommended                       = func(uID int) string { return EndpointUsersV1 + strconv.Itoa(uID) + "/follow_recommended" }
 	EndpointUsersResetCounters                           = func() string { return EndpointUsersV1 + "reset_counters" }
 	EndpointUsersTimestamp                               = func() string { return EndpointUsersV2 + "timestamp" }
-	EndpointUsers                                        = func(uID string) string { return EndpointUsersV2 + uID }
+	EndpointUsers                                        = func(uID int) string { return EndpointUsersV2 + strconv.Itoa(uID) }
 	EndpointUsersCustomDefinitions                       = func() string { return EndpointUsersV1 + "custom_definitions" }
-	EndpointUsersFollowers                               = func(uID string) string { return EndpointUsersV2 + uID + "/followers" }
-	EndpointUsersListFollowings                          = func(uID string) string { return EndpointUsersV2 + uID + "/list_followings" }
+	EndpointUsersFollowers                               = func(uID int) string { return EndpointUsersV2 + strconv.Itoa(uID) + "/followers" }
+	EndpointUsersListFollowings                          = func(uID int) string { return EndpointUsersV2 + strconv.Itoa(uID) + "/list_followings" }
 	EndpointUsersQrCodes                                 = func(qr string) string { return EndpointUsersV1 + "qr_codes/" + qr }
 	EndpointUsersInterests                               = func() string { return EndpointUsersV1 + "interests" }
-	EndpointUsersInfo                                    = func(uID string) string { return EndpointUsersV2 + "info/" + uID }
+	EndpointUsersInfo                                    = func(uID int) string { return EndpointUsersV2 + "info/" + strconv.Itoa(uID) }
 	EndpointUsersListId                                  = func() string { return EndpointUsersV1 + "list_id" }
 	EndpointUsersRemoveUserAvatar                        = func() string { return EndpointUsersV2 + "remove_profile_photo" }
 	EndpointUsersRemoveUserCover                         = func() string { return EndpointUsersV2 + "remove_cover_image" }
@@ -69,16 +76,18 @@ var (
 	EndpointUsersLobiFriends                             = func() string { return EndpointV1 + "lobi_friends" }
 	EndpointUsersSearch                                  = func() string { return EndpointUsersV1 + "search" }
 	EndpointUsersAdditonalMotificationSetting            = func() string { return EndpointUsersV1 + "additonal_notification_setting" }
-	EndpointUsersEnditV2                                 = func() string { return EndpointUsersV2 + "edit" }
+	EndpointUsersEditV2                                  = func() string { return EndpointUsersV2 + "edit" }
 	EndpointUsersVisibleOnSnsFriendRecommendationSetting = func() string { return EndpointUsersV1 + "visible_on_sns_friend_recommendation_setting" }
-	EndpointUsersFollowRequest                           = func(tID string) string { return EndpointUsersV2 + tID + "/follow_request" }
+	EndpointUsersFollowRequest                           = func(uID int) string { return EndpointUsersV2 + strconv.Itoa(uID) + "/follow_request" }
 	EndpointUsersHima                                    = func() string { return EndpointUsersV1 + "hima" }
-	EndpointUsersUnFollow                                = func(uID string) string { return EndpointUsersV2 + uID + "/unfollow" }
+	EndpointUsersUnFollow                                = func(uID int) string { return EndpointUsersV2 + strconv.Itoa(uID) + "/unfollow" }
 	EndpointUsersInviteContact                           = func() string { return EndpointUsersV1 + "invite_contact" }
 	EndpointUsersLanguage                                = func() string { return EndpointUsersV1 + "language" }
 	EndpointUsersEditV3                                  = func() string { return EndpointUsersV3 + "edit" }
-	EndpointUsersBookmarks                               = func(uID string, bID string) string { return EndpointUsersV1 + uID + "/bookmarks/" + bID }
-	EndpointUsersGetBookmark                             = func(uID string) string { return EndpointUsersV1 + uID + "/bookmarks" }
+	EndpointUsersBookmarks                               = func(uID int, pID int) string {
+		return EndpointUsersV1 + strconv.Itoa(uID) + "/bookmarks/" + strconv.Itoa(pID)
+	}
+	EndpointUsersGetBookmark = func(uID int) string { return EndpointUsersV1 + strconv.Itoa(uID) + "/bookmarks" }
 
 	EndpointPostsNewConferenceCall      = func() string { return EndpointPostsV2 + "new_conference_call" }
 	EndpointPostsGroupPinnedPost        = func() string { return EndpointPostsV2 + "group_pinned_post" }
@@ -87,16 +96,16 @@ var (
 	EndpointPostsNewSharePost           = func() string { return EndpointPostsV2 + "new_share_post" }
 	EndpointPostsDeleteAllPost          = func() string { return EndpointPostsV1 + "delete_all_post" }
 	EndpointPostsCallTimeline           = func() string { return EndpointPostsV2 + "call_timeline" }
-	EndpointGetConversation             = func(cID string) string { return EndpointConversationsV2 + cID }
+	EndpointGetConversation             = func(cID int) string { return EndpointConversationsV2 + strconv.Itoa(cID) }
 	EndpointConversationRootPosts       = func() string { return EndpointConversationsV2 + "root_posts" }
 	EndpointPostsCallFollowersTimeline  = func() string { return EndpointPostsV2 + "call_followers_timeline" }
 	EndpointPostsFollowingTimeline      = func() string { return EndpointPostsV2 + "following_timeline" }
 	EndpointPostsGroupTimeline          = func() string { return EndpointPostsV2 + "group_timeline" }
 	EndpointPostsTag                    = func(tag string) string { return EndpointPostsV2 + "tags/" + tag }
 	EndpointPostsMin                    = func() string { return EndpointPostsV2 + "mine" }
-	EndpointPostsGetPost                = func(pID string) string { return EndpointPostsV2 + pID }
-	EndpointPostsLikers                 = func(pID string) string { return EndpointPostsV1 + pID + "/likers" }
-	EndpointPostsRePosts                = func(pID string) string { return EndpointPostsV2 + pID + "/reposts" }
+	EndpointPostsGetPost                = func(pID int) string { return EndpointPostsV2 + strconv.Itoa(pID) }
+	EndpointPostsLikers                 = func(pID int) string { return EndpointPostsV1 + strconv.Itoa(pID) + "/likers" }
+	EndpointPostsRePosts                = func(pID int) string { return EndpointPostsV2 + strconv.Itoa(pID) + "/reposts" }
 	EndpointPostsMultiple               = func() string { return EndpointPostsV2 + "multiple" }
 	EndpointPostsRecentEngagement       = func() string { return EndpointPostsV2 + "recent_engagement" }
 	EndpointPostsRecommendedTag         = func() string { return EndpointPostsV1 + "recommended_tag" }
@@ -107,10 +116,10 @@ var (
 	EndpointPostsUserTimeline           = func() string { return EndpointPostsV2 + "user_timeline" }
 	EndpointPostsLike                   = func() string { return EndpointPostsV2 + "like" }
 	EndpointPostsMassDestroy            = func() string { return EndpointPostsV2 + "mass_destroy" }
-	EndpointPostsUnLike                 = func(pID string) string { return EndpointPostsV2 + pID + "/unlike" }
-	EndpointPostsUpdate                 = func(pID string) string { return EndpointPostsV3 + pID }
-	EndpointPostsRecommendationFeedback = func(pID string) string { return EndpointPostsV2 + pID + "/recommendation_feedback" }
+	EndpointPostsUnLike                 = func(pID int) string { return EndpointPostsV2 + strconv.Itoa(pID) + "/unlike" }
+	EndpointPostsUpdate                 = func(pID int) string { return EndpointPostsV3 + strconv.Itoa(pID) }
+	EndpointPostsRecommendationFeedback = func(pID int) string { return EndpointPostsV2 + strconv.Itoa(pID) + "/recommendation_feedback" }
 	EndpointPostsValidate               = func() string { return EndpointPostsV1 + "validate" }
-	EndpointPostsVideoView              = func(vID string) string { return EndpointPostsV1 + "/videos" + vID + "/view" }
-	EndpointVoteSurvey                  = func(sID string) string { return EndpointV2 + "surveys" + sID + "/vote" }
+	EndpointPostsVideoView              = func(vID int) string { return EndpointPostsV1 + "/videos" + strconv.Itoa(vID) + "/view" }
+	EndpointVoteSurvey                  = func(sID int) string { return EndpointV2 + "surveys" + strconv.Itoa(sID) + "/vote" }
 )
