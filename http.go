6d55fe6db4f3b3722d56b2c7635bde7bb8e2595e
ekkx <geniusnaru@gmail.com>
2024-09-06 19:42:11 +0900

    feat: add http request builder

--- http.go
+++ http.go
@@ -3,50 +3,18 @@ package yaygo
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/google/go-querystring/query"
 )
 
-var (
-	ErrUnknown                    = errors.New("ErrUnknown")
-	ErrInvalidParameter           = errors.New("ErrInvalidParameter")
-	ErrRegisteredUser             = errors.New("ErrRegisteredUser")
-	ErrAccessTokenExpired         = errors.New("ErrAccessTokenExpired")
-	ErrScreenNameAlreadyBeenTaken = errors.New("ErrScreenNameAlreadyBeenTaken")
-	ErrUserNotFound               = errors.New("ErrUserNotFound")
-
-	ErrStatusBadRequest          = errors.New("ErrStatusBadRequest")
-	ErrStatusUnauthorized        = errors.New("ErrStatusUnauthorized")
-	ErrStatusForbidden           = errors.New("ErrStatusForbidden")
-	ErrStatusNotFound            = errors.New("ErrStatusNotFound")
-	ErrStatusInternalServerError = errors.New("ErrStatusInternalServerError")
-)
-
-func mapError(errResp *ErrorResponse) error {
-	switch errResp.ErrorCode {
-	case 0:
-		return fmt.Errorf("%w: %s", ErrUnknown, errResp.Message)
-	case -1:
-		return fmt.Errorf("%w: %s", ErrInvalidParameter, errResp.Message)
-	case -2:
-		return fmt.Errorf("%w: %s", ErrRegisteredUser, errResp.Message)
-	case -3:
-		return fmt.Errorf("%w: %s", ErrAccessTokenExpired, errResp.Message)
-	case -4:
-		return fmt.Errorf("%w: %s", ErrScreenNameAlreadyBeenTaken, errResp.Message)
-	case -5:
-		return fmt.Errorf("%w: %s", ErrUserNotFound, errResp.Message)
-	default:
-		return fmt.Errorf("unknown error code %d: %s", errResp.ErrorCode, errResp.Message)
-	}
-}
-
-func (s *Session) baseRequest(method, url string, params, body interface{}) ([]byte, error) {
+func (s *Session) buildRequest(method, url string, params, body interface{}, jwt bool) (*http.Request, error) {
 	if params != nil {
 		v, err := query.Values(params)
 		if err != nil {
@@ -60,12 +28,58 @@ func (s *Session) baseRequest(method, url string, params, body interface{}) ([]b
 		return nil, err
 	}
 
-	slog.Debug(fmt.Sprintf("[REQ] %s :: %s", method, url))
-
 	req, err := http.NewRequest(method, url, bytes.NewBuffer(jsonBody))
 	if err != nil {
 		return nil, err
 	}
+
+	if s.state.IP == "" && !strings.Contains(url, EndpointUsersTimestamp()) {
+		resp, err := s.User.GetTimestamp()
+		if err != nil {
+			return nil, err
+		}
+		s.state.IP = resp.IPAddress
+	}
+
+	ua := "android 11 (3.5x 1440x2960 Galaxy S9)"
+	// We are not using req.Header.Set() to avoid header key conversion.
+	req.Header = http.Header{
+		"Host":               []string{EndpointHost},
+		"User-Agent":         []string{ua},
+		"X-Timestamp":        []string{strconv.FormatInt(time.Now().Unix(), 10)},
+		"X-App-Version":      []string{API_VERSION_NAME},
+		"X-Device-Info":      []string{fmt.Sprintf("yay %s %s", VERSION_NAME, ua)},
+		"X-Device-UUID":      []string{s.state.DeviceUUID},
+		"X-Client-IP":        []string{s.state.IP},
+		"X-Connection-Type":  []string{"wifi"},
+		"X-Connection-Speed": []string{"0kbps"},
+		"Accept-Language":    []string{"ja"},
+		"Content-Type":       []string{"application/json;charset=UTF-8"},
+	}
+
+	if jwt {
+		req.Header["X-Jwt"] = []string{"TODO: generateJWT()"}
+	}
+
+	if s.state.AccessToken != "" {
+		req.Header["Authorization"] = []string{fmt.Sprintf("Bearer %s", s.state.AccessToken)}
+	}
+
+	return req, nil
+}
+
+func (s *Session) baseRequest(method, url string, params, body interface{}, jwt bool) ([]byte, error) {
+	req, err := s.buildRequest(method, url, params, body, jwt)
+	if err != nil {
+		return nil, err
+	}
+
+	slog.Debug(fmt.Sprintf("[REQ] %s :: %s", method, req.URL))
+	for k, v := range req.Header {
+		slog.Debug(fmt.Sprintf("[REQ] HEADERS %s: %s", k, v))
+	}
+	slog.Debug(fmt.Sprintf("[REQ] %s", req.Body))
+	slog.Debug("[FET] ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓")
 
 	resp, err := s.client.Do(req)
 	if err != nil {
@@ -78,7 +92,10 @@ func (s *Session) baseRequest(method, url string, params, body interface{}) ([]b
 		return nil, err
 	}
 
-	slog.Debug(fmt.Sprintf("[RES] %s :: %s", resp.Status, byteResp))
+	for k, v := range resp.Header {
+		slog.Debug(fmt.Sprintf("[RES] HEADERS %s: %s", k, v))
+	}
+	slog.Debug(fmt.Sprintf("[RES] %s :: %s\n\n\n", resp.Status, byteResp))
 
 	var errResp ErrorResponse
 	if err := json.Unmarshal(byteResp, &errResp); err == nil && errResp.Result == "error" {
@@ -107,6 +124,6 @@ func (s *Session) baseRequest(method, url string, params, body interface{}) ([]b
 	return byteResp, nil
 }
 
-func (s *Session) request(method, url string, params, body interface{}) ([]byte, error) {
-	return s.baseRequest(method, url, params, body)
+func (s *Session) request(method, url string, params, body interface{}, jwt bool) ([]byte, error) {
+	return s.baseRequest(method, url, params, body, jwt)
 }
