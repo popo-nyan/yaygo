ec1cdf622a5bb9e0da5f6d2c048814a3c1588584
ekkx <geniusnaru@gmail.com>
2024-09-06 19:43:26 +0900

    feat: add login responses

--- models.go
+++ models.go
@@ -5,3 +5,12 @@ type User struct {
 	ID        int    `json:"id"`
 	Nickname  string `json:"nickname"`
 }
+
+type SNSInfo struct {
+	Biography    string `json:"biography"`
+	Gender       string `json:"gender"`
+	Nickname     string `json:"nickname"`
+	ProfileImage string `json:"profile_image"`
+	Type         string `json:"type"`
+	UID          string `json:"uid"`
+}
--- responses.go
+++ responses.go
@@ -12,6 +12,24 @@ type ErrorResponse struct {
 	RetryIn   int    `json:"retry_in"`
 }
 
+type LoginUserResponse struct {
+	Response
+	AccessToken  string  `json:"access_token"`
+	ExpiresIn    int     `json:"expires_in"`
+	IsNew        bool    `json:"is_new"`
+	RefreshToken string  `json:"refresh_token"`
+	SNSInfo      SNSInfo `json:"sns_info"`
+	UserID       int     `json:"user_id"`
+	Username     string  `json:"username"`
+}
+
+type UserTimestampResponse struct {
+	Response
+	Country   string `json:"country"`
+	IPAddress string `json:"ip_address"`
+	Time      int    `json:"time"`
+}
+
 type UserResponse struct {
 	Response
 	User User `json:"user"`
