e53f0447325988615d6817c006a21c46f065923a
ekkx <geniusnaru@gmail.com>
2024-09-06 19:44:30 +0900

    feat: impl state storage to session

--- session.go
+++ session.go
@@ -1,14 +1,26 @@
 package yaygo
 
 import (
+	"log/slog"
 	"net/http"
+	"os"
 	"time"
 )
 
-const VERSION = "0.0.1"
+const (
+	VERSION = "0.0.1"
+
+	API_VERSION_NAME = "3.51"
+	VERSION_NAME     = "3.51.0"
+	API_VERSION_KEY  = "8befe6044b504adca957d5c4fb5dba86"
+	API_KEY          = "ccd59ee269c01511ba763467045c115779fcae3050238a252f1bd1a4b65cfec6"
+)
 
 type Session struct {
+	baseDir string
+
 	client *http.Client
+	state  *State
 
 	Auth         *AuthApi
 	Call         *CallApi
@@ -24,11 +36,16 @@ type Session struct {
 }
 
 type SessionConfig struct {
+	baseDir string
+
 	client *http.Client
 }
 
+// Default session configuration.
 func newSessionConfig() *SessionConfig {
 	return &SessionConfig{
+		baseDir: ".config/",
+
 		client: &http.Client{Timeout: (20 * time.Second)},
 	}
 }
@@ -43,14 +60,25 @@ func WithClient(client *http.Client) SessionOption {
 	}
 }
 
-func New(email, password string, options ...SessionOption) (s *Session, err error) {
+func WithBaseDir(baseDir string) SessionOption {
+	return func(cfg *SessionConfig) {
+		if baseDir != "" {
+			cfg.baseDir = baseDir
+		}
+	}
+}
+
+func New(email, password string, options ...SessionOption) (*Session, error) {
 	cfg := newSessionConfig()
 	for _, opt := range options {
 		opt(cfg)
 	}
 
-	s = &Session{
-		client: &http.Client{Timeout: (20 * time.Second)},
+	s := &Session{
+		baseDir: cfg.baseDir,
+
+		client: cfg.client,
+		state:  newState(cfg.baseDir, email),
 	}
 
 	s.Auth = newAuthApi(s)
@@ -65,5 +93,44 @@ func New(email, password string, options ...SessionOption) (s *Session, err erro
 	s.Thread = newThreadApi(s)
 	s.User = newUserApi(s)
 
-	return
+	if err := s.init(email, password); err != nil {
+		return nil, err
+	}
+
+	return s, nil
+}
+
+func (s *Session) init(email, password string) error {
+	if _, err := os.Stat(s.baseDir); os.IsNotExist(err) {
+		return os.MkdirAll(s.baseDir, 0700)
+	}
+
+	if err := s.state.Read(); os.IsNotExist(err) {
+		slog.Debug("State file not found. Creating new one.")
+		err := s.state.Write()
+		if err != nil {
+			return err
+		}
+	}
+
+	if s.state.IsUnauthorized() {
+		resp, err := s.Auth.Login(&LoginParams{Email: email, Password: password})
+		if err != nil {
+			return err
+		}
+
+		s.state.AccessToken = resp.AccessToken
+		s.state.RefreshToken = resp.RefreshToken
+		s.state.UserID = resp.UserID
+
+		slog.Debug("Saving authenticated state.")
+
+		if err := s.state.Write(); err != nil {
+			return err
+		}
+	}
+
+	slog.Debug("Sesssion intialized.")
+
+	return nil
 }
