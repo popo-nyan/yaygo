3fe281415799314666da0af39d9297f9c02c7137
MocA-Love <raimu_hero@yahoo.co.jp>
2024-09-07 00:05:02 +0900

    structtttt

--- chat.go
+++ chat.go
@@ -120,3 +120,223 @@ func (c *ChatApi) Edit(params *EditParams) (st * Response, err error) {
 	return
 }
 
+
+type GetChatableUsersParams struct {
+	Request        SearchUsersRequest `json:"request,omitempty"`
+	From_follow_id int                `json:"from_follow_id,omitempty"`
+	From_timestamp int                `json:"from_timestamp,omitempty"`
+	Order_by       string             `json:"order_by,omitempty"`
+}
+
+
+
+
+type GetGifsDataParams struct {}
+
+
+
+
+
+
+
+type GetHiddenChatRoomsParams struct {
+	Number int         `json:"number,omitempty"`
+	From_timestamp int `json:"from_timestamp,omitempty"`
+}
+
+
+
+
+
+
+type GetMainRoomsParams struct {
+	From_timestamp int `json:"from_timestamp,omitempty"`
+}
+
+
+
+
+type GetMessagesParams struct {
+	Id              int `json:"id,omitempty"`
+	Number          int `json:"number,omitempty"`
+	From_message_id int `json:"from_message_id,omitempty"`
+	To_message_id   int `json:"to_message_id,omitempty"`
+}
+
+
+
+
+type GetNotificationSettingsParams struct {
+	Id int `json:"id,omitempty"`
+}
+
+
+
+
+type GetRequestRoomsParams struct {
+	From_timestamp int `json:"from_timestamp,omitempty"`
+}
+
+
+
+
+type GetRoomParams struct {
+	Id int `json:"id,omitempty"`
+}
+
+
+
+
+type GetStickerPacksParams struct {}
+
+
+
+
+type GetTotalRequestsParams struct {}
+
+
+
+
+type HideChatParams struct {
+	Id int `json:"id,omitempty"`
+}
+
+
+
+
+type InviteParams struct {
+	Id            int   `json:"id,omitempty"`
+	With_user_ids []int `json:"with_user_ids[],omitempty"`
+}
+
+
+
+type KickUsersParam struct {
+	Id            int   `json:"id,omitempty"`
+	With_user_ids []int `json:"with_user_ids[],omitempty"`
+}
+
+
+type PinParams struct {
+	Id int `json:"id,omitempty"`
+}
+
+
+
+
+type ReadAttachmentParams struct {
+	Id      int                   `json:"id,omitempty"`
+	Request ReadAttachmentRequest `json:"request,omitempty"`
+}
+
+
+
+
+type ReadMessageParams struct {
+	Id         int `json:"id,omitempty"`
+	Message_id int `json:"message_id,omitempty"`
+}
+
+
+
+
+type ReadVideoMessageParams struct {
+	Id            int `json:"id,omitempty"`
+	Video_msg_ids []int `json:"video_msg_ids[],omitempty"`
+	Message_id    int `json:"message_id,omitempty"`
+}
+
+
+
+
+type RefreshRoomsParams struct {
+	From_timestamp int `json:"from_timestamp,omitempty"`
+}
+
+
+
+
+type RemoveParams struct {
+	Chat_room_ids []int `json:"chat_room_ids[],omitempty"`
+	Id            int `json:"id,omitempty"`
+}
+
+
+
+
+type ReportParams struct {
+	Chat_room_id          []int   `json:"chat_room_id,omitempty"`
+	Category_id           int     `json:"category_id,omitempty"`
+	Reason                string  `json:"reason,omitempty"`
+	Opponent_id           int     `json:"opponent_id,omitempty"`
+	Screenshot_filename   string  `json:"screenshot_filename,omitempty"`
+	Screenshot_2_filename string  `json:"screenshot_2_filename,omitempty"`
+	Screenshot_3_filename string  `json:"screenshot_3_filename,omitempty"`
+	Screenshot_4_filename string  `json:"screenshot_4_filename,omitempty"`
+}
+
+
+
+
+
+
+type SendMediaScreenshotNotificationParams struct {
+	Id int `json:"id,omitempty"`
+}
+
+
+
+
+
+
+
+type SendMessageParams struct {
+	Id                   int    `json:"id,omitempty"`
+	Message_type         string `json:"message_type,omitempty"`
+	Call_type            string `json:"call_type,omitempty"`
+	Text                 string `json:"text,omitempty"`
+	Font_size            int    `json:"font_size,omitempty"`
+	Gif_image_id         int    `json:"gif_image_id,omitempty"`
+	Attachment_file_name string `json:"attachment_file_name,omitempty"`
+	Sticker_pack_id      int    `json:"sticker_pack_id,omitempty"`
+	Sticker_id           int    `json:"sticker_id,omitempty"`
+	Video_file_name      string `json:"video_file_name,omitempty"`
+	Parent_id            int    `json:"parent_id,omitempty"`
+}
+
+
+
+
+
+
+
+type SetNotificationSettingsParams struct {
+	Id                int `json:"id,omitempty"`
+	Notification_chat int `json:"notification_chat,omitempty"`
+}
+
+
+
+
+
+
+
+type UnHideChatParams struct {
+	Chat_room_ids int `json:"chat_room_ids,omitempty"`
+}
+
+
+
+
+
+
+type UnpinParams struct {
+	Id int `json:"id,omitempty"`
+
+
+
+
+
+
+
+
